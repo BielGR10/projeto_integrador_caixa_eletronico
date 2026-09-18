fe3fd919e5e25e550091c18a280ee26d1d9f3a4f
BielGR10 <gabriel.alves.martins.2010@gmail.com>
2026-09-18 15:57:31 -0300

    Feat: Sistema de caixa elêtronico feito

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,60 @@
 package org.example;
-
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        Scanner input = new Scanner(System.in);
+        int escolha = 0;
+        float saldoConta = 0f, deposito = 0f, saque = 0f;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        do {
+            System.out.println("""
+                    ----------CAIXA ELETRONIC0----------
+                    1 - Consultar Saldo
+                    2 - Realizar Depósito
+                    3 - Realizar Saque
+                    4 - Sair
+                    """);
+            escolha = input.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.print("Seu saldo é de: \nR$"+saldoConta+"\n");
+                    break;
+
+                case 2:
+                    System.out.print("Qual valor do depósito desejado? \nR$");
+                    deposito = input.nextFloat();
+                    if (deposito <= 0){
+                        System.out.println("Depósito inválido.\n");
+                    }else {
+                        saldoConta += deposito;
+                        System.out.println("Depósito de R$"+deposito+" feito.\n");
+
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Qual valor do saque desejado? \nR$");
+                    saque = input.nextFloat();
+                    if (saque > saldoConta){
+                        System.out.println("Saldo insuficiente.\n");
+                    }else {
+                        saldoConta -= saque;
+                        System.out.println("Saque sucedido.\n");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Sistema encerrando.");
+                    break;
+
+                default:
+                    System.out.println("OPÇÃO INVÁLIDA, TENTE NOVAMENTE.\n");
+                    break;
+            }
+        }while (escolha != 4);
+
     }
 }
